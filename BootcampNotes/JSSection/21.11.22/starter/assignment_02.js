123d966ee0fc347d8c0c3d76fa95b292438b5e1d
Andrii Nehrov <andrey.nehrov@gmail.com>
2022-11-21 17:34:42 +0100

    working with operators, consts

--- BootcampNotes/JSSection/21.11.22/starter/assignment_02.js
+++ BootcampNotes/JSSection/21.11.22/starter/assignment_02.js
@@ -1,5 +1,16 @@
+const country = 'DE';
+const continent = 'EUROPE';
+let population = 82900000;
+
 let isIsland = false;
-let language;
+const language = 'german';
 
 console.log(typeof isIsland);
 console.log(typeof language);
+
+console.log(population++);
+
+let finlandPopulation = 6000000;
+let isDEBiggerThanFI = population > finlandPopulation
+
+console.log(isDEBiggerThanFI);
--- BootcampNotes/JSSection/21.11.22/starter/script.js
+++ BootcampNotes/JSSection/21.11.22/starter/script.js
@@ -27,4 +27,43 @@ BitInt
 // console.log(typeof(null)) //a bug?
 
 let age = 30;
-age = 31;
+age = 31; //mutating a variable
+
+const birthYear = 1991; //immutable variable
+//birthYear = 1992;
+
+//const HAS to be initialized when created
+
+//lastName = 'My Name'; //scope of the code
+
+const now = 2022
+const ageAndrew = 2037 - now;
+const ageFriend = 2044 - now;
+
+console.log(ageAndrew, ageFriend);
+
+console.log(ageAndrew / 2, ageFriend * 2);
+console.log(2 ** 3);
+
+const fristName = 'Andrew';
+const lastName = 'Nero';
+
+console.log(fristName + ' ' + lastName);
+
+let x = 10 + 5;
+console.log(x);
+
+x += 22;
+console.log(x);
+
+x *= 2;
+console.log(x);
+
+x++;
+console.log(x);
+
+console.log(3>2);
+console.log(ageAndrew <= 18);
+
+const isMature = ageAndrew >= 18;
+console.log(isMature);
