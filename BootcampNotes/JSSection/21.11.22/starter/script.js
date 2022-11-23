942a70ed684f359156abe8f8548ab729cc63988b
Andrii Nehrov <andrey.nehrov@gmail.com>
2022-11-23 18:44:41 +0100

    fizzbuzz

--- BootcampNotes/JSSection/21.11.22/starter/script.js
+++ BootcampNotes/JSSection/21.11.22/starter/script.js
@@ -1,26 +1,26 @@
-// let z, y;
-// z = y = 10 - 10 - 10; //Assignment is RIGHT to LEFT
-// console.log(z, y);
+// // let z, y;
+// // z = y = 10 - 10 - 10; //Assignment is RIGHT to LEFT
+// // console.log(z, y);
 
-const fName = 'Jonas';
-const job = 'teacher';
-const birthYear = 1991;
+// const fName = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
 
-const jonas = `He's ${fName} a 46-year-old ${job}`;
-console.log(jonas);
+// const jonas = `He's ${fName} a 46-year-old ${job}`;
+// console.log(jonas);
 
-console.log(`First line
-Second
-Thrid`);
+// console.log(`First line
+// Second
+// Thrid`);
 
-//--------------------------------------
+// //--------------------------------------
 
-const age = 19;
-const isOldEnough = age >= 18;
+// const age = 19;
+// const isOldEnough = age >= 18;
 
-if (isOldEnough) {
-    console.log('He can drive.')
-} else {
-    const yearsLeft = 18 - age;
-    console.log(`Wait ${yearsLeft} more years.`);
-}
+// if (isOldEnough) {
+//     console.log('He can drive.')
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Wait ${yearsLeft} more years.`);
+// }
