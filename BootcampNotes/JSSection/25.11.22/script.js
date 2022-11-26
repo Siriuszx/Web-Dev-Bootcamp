65b36463e40da36df18a986bdebf24017814c47e
Andrii Nehrov <andrey.nehrov@gmail.com>
2022-11-26 20:14:41 +0100

    arrow function

--- BootcampNotes/JSSection/25.11.22/script.js
+++ BootcampNotes/JSSection/25.11.22/script.js
@@ -1,33 +1,48 @@
 'use strict';
 
-function logger(text) {
-    console.log(text)
+// function logger(text) {
+//     console.log(text)
+// }
+
+// // calling/invoking/running function
+
+// logger('This is a function, nice');
+
+// function getDouble(num) {
+//     return num * 2;
+// }
+
+// const four = getDouble(2);
+
+// console.log(`The two by two is ${four}`);
+
+// //Declaration
+// function calcAge(birthYear) {
+//     return 2022 - birthYear;
+// }
+
+// let age = calcAge(2002);
+
+// //Expression
+// const calcAge2 = function (birthYear) {
+//     return 2022 - birthYear;
+// }
+
+// let age2 = calcAge2(2002);
+
+// console.log(age, age2);
+
+// Arrow function
+
+const calcAge3 = birthYear => 2022 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2022 - birthYear;
+    const retirementAge = 65 - age;
+    return `${firstName} retires at ${retirementAge}.`;
 }
 
-// calling/invoking/running function
-
-logger('This is a function, nice');
-
-function getDouble(num) {
-    return num * 2;
-}
-
-const four = getDouble(2);
-
-console.log(`The two by two is ${four}`);
-
-//Declaration
-function calcAge(birthYear) {
-    return 2022 - birthYear;
-}
-
-let age = calcAge(2002);
-
-//Expression
-const calcAge2 = function (birthYear) {
-    return 2022 - birthYear;
-}
-
-let age2 = calcAge2(2002);
-
-console.log(age, age2);
+console.log(yearsUntilRetirement(2002,'Andrew'));
+console.log(yearsUntilRetirement(1991,'Alex'));
