f541e156ce5bfda28dcfba2676228688e05a5430
Andrii Nehrov <andrey.nehrov@gmail.com>
2022-11-30 22:21:19 +0100

    js objects

---
+++ BootcampNotes/JSSection/30.11.22/script.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName: 'Alex',
+    lastName: 'E.',
+    age: 20,
+    job: 'web developer'
+};
+
+console.log(person);
+console.log(person.firstName);
+console.log(person['firstName'] ,person['lastName']);
