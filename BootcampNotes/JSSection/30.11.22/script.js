881146ffe030d154c3781569610e18c8e0d82828
Andrii Nehrov <andrey.nehrov@gmail.com>
2022-12-01 19:11:43 +0100

    obj methods, challenge

--- BootcampNotes/JSSection/30.11.22/script.js
+++ BootcampNotes/JSSection/30.11.22/script.js
@@ -1,10 +1,50 @@
-const person = {
-    firstName: 'Alex',
-    lastName: 'E.',
-    age: 20,
-    job: 'web developer'
-};
+// const person = {
+//     firstName: 'Alex',
+//     lastName: 'E.',
+//     birthYear: 2002,
+//     job: 'web developer',
+//     hasDriversLicense: true,
+//     getAge: function(curYear) {
+//         return curYear - this.birthYear;
+//     },
+//     getSummary: function() {
+//         return `Name: ${this.firstName} ${this.lastName}\nAge: ${this.getAge(2002)}\nHas driver's license: ${this.hasDriversLicense}`;
+//     }
+// };
 
-console.log(person);
-console.log(person.firstName);
-console.log(person['firstName'] ,person['lastName']);
+// console.log(person);
+// console.log(person.firstName);
+// console.log(person['firstName'] ,person['lastName'],person.getAge(2022));
+// console.log(person.getSummary());
+
+let mark = {
+    name: 'Mark',
+    massKG: 78,
+    heightM: 1.69,
+    BMI: null,
+    getBMI: function () {
+        this.BMI = this.massKG / this.heightM ** 2;
+        return this.BMI;
+    }
+}
+
+let john = {
+    name: 'John',
+    massKG: 92,
+    heightM: 1.95,
+    BMI: null,
+    getBMI: function () {
+        this.BMI = this.massKG / this.heightM ** 2;
+        return this.BMI;
+    }
+}
+
+let hasHigherBMI = function (mark, john) {
+    if (mark.getBMI() > john.getBMI()) {
+        console.log(`Mark has higher BMI: ${mark.getBMI()} vs ${john.getBMI()}`)
+    } else {
+        console.log(`John has higher BMI: ${john.getBMI()} vs ${mark.getBMI()}`)
+    }
+}
+
+hasHigherBMI(mark,john);
